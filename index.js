bf317831c8c79331915ebc8d36128d80226d54fd
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2019-04-02 19:08:06 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('#button')
 button.addEventListener('click', function(event) {
 	console.log('elo');
-	navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+	navigator.bluetooth.requestDevice({ filters: [{ name: 'Kontakt-8MqY' }] })
 		.then(console.log)
 		.catch(console.error)
   });
