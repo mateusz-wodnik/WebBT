060109a5109d87099246620772a9e64aeb425bff
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2019-04-02 21:55:40 +0200

    add service handler

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ button.addEventListener('click', function(event) {
 		.catch(err => console.error('connection', err))
 		.then(server => {
 			console.log('server', server);
+			return server.getPrimaryService(0x1800);
+		})
+		.then(service => {
+			console.log(service);
+			console.log('elo :)')
 		})
 		.catch(err => console.error('server', err))
-		
+
   });
   
