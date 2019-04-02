eda461965b657efbcb4fc191ed4426e8e3e5e312
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2019-04-02 21:50:15 +0200

    update connection

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const button = document.querySelector('#button')
 button.addEventListener('click', function(event) {
-	console.log('elo');
-	navigator.bluetooth.requestDevice({ filters: [{ name: 'Kontakt-8MqY' }] })
-		.then(console.log)
-		.catch(console.error)
+	console.log('mordo');
+	navigator.bluetooth.requestDevice({ acceptAllDevices: true })
+		.then(device => {
+			console.log('device', device);
+			device.addEventListener('gattserverdisconnected', () => console.log('REEEEEEEEEEEEEEEEEEE'));
+			return device.gatt.connect();
+		})
+		.catch(err => console.error('connection', err))
+		.then(server => {
+			console.log('server', server);
+		})
+		.catch(err => console.error('server', err))
+		
   });
   
